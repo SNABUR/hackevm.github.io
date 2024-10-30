9ffbc9d9feed420d9491c5c36acd538f4be5a8f1
SNABUR <promisesimetry@gmail.com>
2024-10-30 16:48:13 -0500

    xd

--- src/app/eth/page.tsx
+++ src/app/eth/page.tsx
@@ -1,115 +1,136 @@
-// pages/index.js
 "use client";
 import { useState } from 'react';
-import { keccak256 } from 'js-sha3';
-import { ethers } from 'ethers';
+import { generatePrivateKey, generateBTCAddress } from '../../utils/bitcoinUtilss'; // Importa las funciones desde utils
 import copy from '../../images/copy.svg';
-import etherscan from '../../images/etherscan_logo.svg';
-import eth from '../../images/ethereum.svg';
-import Image from 'next/image';
+import blockchain from '../../images/blockchain.svg';
+import btc from '../../images/Bitcoin.svg';
 import { FaEye, FaEyeSlash } from 'react-icons/fa';
+import Image from 'next/image';
 
-const ETHPage = () => {
-  const [inputText, setInputText] = useState('vitalik');
-  const [ethAddress, setEthAddress] = useState('0x89d768F75bd1Ae465876046d5e5466D0b1FdbD03');
-  const [privateKey, setPrivateKey] = useState('0xaf2caa1c2ca1d027f1ac823b529d0a67cd144264b2789fa2ea4d63a67c7103cc');
+const BTCPage = () => {
+  const [inputText, setInputText] = useState('Satoshi Nakamoto');
+  const [BTCAddress, setBTCAddress] = useState('17ZYZASydeA1xyfNrcYcLyqghmK3eGJpHq');
+  const [privateKey, setPrivateKey] = useState('a0dc65ffca799873cbea0ac274015b9526505daaaed385155425f7337704883e');
   const [showPrivateKey, setShowPrivateKey] = useState(false);
+  const [addressType, setAddressType] = useState('P2PKH'); // Estado para el tipo de dirección
 
-  // Función para generar la clave y dirección cada vez que cambia el texto
+  // Genera clave y dirección BTC cada vez que cambia el texto
   const handleInputChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     const text = e.target.value;
     setInputText(text);
     if (text) {
-      const hashedOutput = keccak256(text);
-      const privateKeyHex = `0x${BigInt(`0x${hashedOutput}`).toString(16).padStart(64, '0')}`;
-      setPrivateKey(privateKeyHex);
-      const publicKey = generatePublicKey(privateKeyHex);
-      setEthAddress(publicKey);
+      generateAddress(text); // Llama a la función para generar la dirección
     } else {
-      // Limpiar dirección y clave si el campo está vacío
-      setEthAddress('');
+      setBTCAddress('');
       setPrivateKey('');
     }
   };
 
-  const generateAddress = () => {
-    // Calcular el hash Keccak-256
-    const hashedOutput = keccak256(inputText);
-
-    // Generar la clave privada a partir del hash
-    const privateKeyHex = `0x${BigInt(`0x${hashedOutput}`).toString(16).padStart(64, '0')}`;
+  // Función para generar dirección y clave BTC
+  const generateAddress = (text: string) => {
+    const privateKeyHex = generatePrivateKey(text); // Genera la clave privada
     setPrivateKey(privateKeyHex);
 
-    // Calcular la dirección Ethereum a partir de la clave privada
-    const publicKey = generatePublicKey(privateKeyHex);
-    setEthAddress(publicKey);
-  };
+    let btcAddress = '';
 
-  const generatePublicKey = (privateKeyHex: string) => {
-    // Usar ethers.js para generar la dirección Ethereum a partir de la clave privada
-    const wallet = new ethers.Wallet(privateKeyHex);
-    return wallet.address;
+    switch (addressType) {
+      case 'P2PKH':
+        btcAddress = generateBTCAddress(privateKeyHex, 'P2PKH');
+        break;
+      case 'BECH32':
+        btcAddress = generateBTCAddress(privateKeyHex, 'BECH32');
+        break;
+      default:
+        throw new Error('Tipo de dirección no soportado');
+    }
+
+    setBTCAddress(btcAddress);
   };
 
   const copyToClipboard = (text: string) => {
-    navigator.clipboard.writeText(text)
-      .catch(err => console.error('Failed to copy text: ', err));
+    navigator.clipboard.writeText(text).catch(err => console.error('Failed to copy text: ', err));
   };
+
+  const handleAddressTypeChange = (type: string) => {
+    setAddressType(type);
+    generateAddress(inputText); // Generar la dirección cada vez que cambia el tipo
+  };
+
 
   return (
     <div className="min-h-screen bg-gray-100 flex items-center font-hacker justify-center">
       <div className="flex flex-col bg-white shadow-lg rounded-lg p-8 w-full max-w-md items-center">
         <div className="flex flex-col items-center mb-6">
-          <Image className='mb-3' src={eth.src} alt="Ethereum Logo" width={64} height={64} />
-          <h1 className="text-3xl font-bold text-gray-800 text-center">Hack Ethereum</h1>
+          <Image className='mb-3' src={btc.src} alt="Bitcoin Logo" width={64} height={64} />
+          <h1 className="text-3xl font-bold text-gray-800 text-center">Hack Bitcoin</h1>
         </div>
-  
+
         <div className="w-full mb-4">
-          <label className="block text-sm font-medium text-gray-700 mb-2">Insert Text:</label>
+          <div className='flex flex-fil text-center justify-between'>
+            <label className="block text-sm font-medium text-gray-700 mb-1">Insert Text:</label>
+            <div className="flex items-center mb-4">
+            <button
+              className={`py-1 px-3 text-xs rounded-l-lg transition duration-200 ${addressType === 'P2PKH' ? 'bg-orange-400 text-white' : 'bg-orange-100 text-black'}`}
+              onClick={() => handleAddressTypeChange('P2PKH')}
+            >
+              A
+            </button>
+            <button
+              className={`py-1 px-3  text-xs rounded-r-lg transition duration-200 ${addressType === 'BECH32' ? 'bg-orange-400 text-white' : 'bg-orange-100 text-black'}`}
+              onClick={() => handleAddressTypeChange('BECH32')}
+            >
+              B
+            </button>
+          </div>
+        </div>
           <textarea
             className="border border-gray-300 rounded-lg p-3 w-full focus:ring-2 h-24 focus:ring-blue-500 text-black text-sm"
             value={inputText}
             onChange={handleInputChange}
-            placeholder="vitalik"
+            placeholder="Satoshi Nakamoto"
           />
         </div>
-  
+
+        {/* Selector de tipo de dirección */}
+
+
+
         <button
-          className="bg-purple-600 text-white py-2 px-4 rounded-lg mt-1 hover:bg-blue-700 transition w-64 active:scale-95 duration-200"
-          onClick={generateAddress}
+          className="bg-orange-400 text-white py-2 px-4 rounded-lg mt-1 hover:bg-blue-700 transition w-64 active:scale-95 duration-200"
+          onClick={() => generateAddress(inputText)} // Llama a la función para generar la dirección
         >
           GENERATE WALLET
         </button>
-  
+
         <div className="w-full mt-6">
           <div className="flex items-center justify-start mb-2">
-            <label className="text-sm font-medium text-gray-700">ETH Address:</label>
+            <label className="text-sm font-medium text-gray-700">BTC Address:</label>
             <button
               className="bg-gray-200 py-1 px-2 rounded-lg hover:bg-gray-300 transition duration-200 active:scale-95"
-              onClick={() => copyToClipboard(ethAddress)}
+              onClick={() => copyToClipboard(BTCAddress)}
             >
               <Image src={copy.src} alt="Copy" width={16} height={16} />
             </button>
           </div>
-  
+
           <div className="flex items-center gap-2 mb-4">
             <textarea
               className="border border-gray-300 rounded-lg p-3 w-full bg-gray-100 text-black"
               readOnly
-              value={ethAddress}
+              value={BTCAddress}
             />
-
-            <a
-              className="p-2 rounded-lg hover:bg-gray-300 transition duration-200 active:scale-95"
-              href={`https://etherscan.io/address/${ethAddress}`}
+                        <a
+              className="rounded-lg hover:bg-gray-300 transition duration-200 active:scale-95"
+              href={`https://www.blockchain.com/explorer/addresses/btc/${BTCAddress}`}
               target="_blank"
               rel="noopener noreferrer"
             >
-              <Image src={etherscan.src} alt="Etherscan" width={26} height={26} />
+              <Image src={blockchain.src} alt="Blockchain" width={26} height={26} />
             </a>
+
           </div>
         </div>
-  
+
         <div className="w-full mt-6">
           <div className="flex items-center justify-start mb-2">
             <label className="text-sm font-medium text-gray-700">Private Key:</label>
@@ -119,8 +140,9 @@ const ETHPage = () => {
             >
               <Image src={copy.src} alt="Copy" width={16} height={16} />
             </button>
+
           </div>
-  
+
           <div className="flex items-center gap-2 mb-4">
             <textarea
               className="border border-gray-300 rounded-lg p-3 w-full bg-gray-100 text-black"
@@ -131,11 +153,10 @@ const ETHPage = () => {
               className="p-1 rounded-xl hover:bg-gray-300 transition duration-200 active:scale-90 focus:outline-none"
               onClick={() => setShowPrivateKey(!showPrivateKey)}
             >
-              {showPrivateKey ? <FaEye className="text-black w-6 h-6" /> : <FaEyeSlash className="text-black w-6 h-6" />}
+            {showPrivateKey ? <FaEye className="text-black w-6 h-6" /> : <FaEyeSlash className="text-black w-6 h-6" />}
             </button>
           </div>
         </div>
-
         <div className="flex flex-col items-center mt-3 text-center">
           <p className="text-sm text-gray-600 mb-2">
           made with ❤️ by GG
@@ -154,9 +175,9 @@ const ETHPage = () => {
           </p>
           <div
             className="flex items-center space-x-1 rounded-lg p-1 cursor-pointer transition duration-200"
-            onClick={() => copyToClipboard("0x3d90Eb79C1e753Ca51D1447791C07e7CcC219e5C")}
+            onClick={() => copyToClipboard("bc1q2qdqw82tqmt6ctt6ydr7lxg3mrg4ps4l6a6m9m")}
           >
-            <span className="text-gray-700 text-xs transition duration-200 active:scale-95">0x3d90Eb79C1e753Ca51D1447791C07e7CcC219e5C</span>
+            <span className="text-gray-700 text-xs transition duration-200 active:scale-95">bc1q2qdqw82tqmt6ctt6ydr7lxg3mrg4ps4l6a6m9m</span>
             <button
               className="p-2 rounded-lg transition duration-200 active:scale-90 focus:outline-none"
               aria-label="Copy to clipboard"
@@ -170,4 +191,4 @@ const ETHPage = () => {
   );
 };
 
-export default ETHPage;
+export default BTCPage;
