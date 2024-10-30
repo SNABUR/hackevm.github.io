53e58e9903f301da01b8543691c8be8b40891c45
Hackfun <manueljonat_159753@hotmail.com>
2024-10-30 07:03:30 -0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
   webpack: (config) => {
-    // Habilitar soporte para WebAssembly y capas
-    config.experiments = {
-      asyncWebAssembly: true,
-      syncWebAssembly: true,
-      layers: true,
-    };
+    config.experiments = { asyncWebAssembly: true, layers: true };
 
-    // Agregar regla para archivos `.wasm`
     config.module.rules.push({
       test: /\.wasm$/,
       type: "webassembly/async",
